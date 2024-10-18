93433b3b7a7255c2fa3a4c467879860d13e8c020
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-10-17 19:29:13 -0500

    Modified to eliminate duplicate jobs being shown in all results.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,7 +112,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -120,7 +120,7 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        if(someJobs != null && !someJobs.isEmpty()){
+        if (someJobs != null && !someJobs.isEmpty()) {
             for (HashMap<String, String> job : someJobs) {
                 System.out.println("\n*****");
                 for (Map.Entry<String, String> set :
@@ -129,8 +129,8 @@ public class TechJobs {
                 }
                 System.out.println("*****");
             }
-        }else{
+        } else {
             System.out.print("No Results");
         }
-   }
+    }
 }
